4bbd963e1ac4a3dadc77dd0447986bff32e75862
Keyvan <skhprogramming@gmail.com>
2024-01-30 19:45:59 +0330

    Disabled ssr for markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,77 +1,79 @@
 'use client';
 
-import { Button, Callout, TextField, Text } from '@radix-ui/themes';
-import { useForm, Controller } from 'react-hook-form';
+import {Button, Callout, TextField, Text} from '@radix-ui/themes';
+import dynamic from "next/dynamic";
+import {useForm, Controller} from 'react-hook-form';
 import axios from 'axios';
-import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
-import { MdErrorOutline } from 'react-icons/md';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
+import {MdErrorOutline} from 'react-icons/md';
+import {useRouter} from 'next/navigation';
+import {useState} from 'react';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {createIssueSchema} from '@/app/validationSchemas';
+import {z} from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {ssr: false})
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const router = useRouter();
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
-  });
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
+    const router = useRouter();
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
+    const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      setLoading(true);
-      await axios.post('/api/issues', data);
-      router.push('/issues');
-    } catch (error) {
-      setLoading(false);
-      setError('An unexpected error occurred!');
-    }
-  });
+    const onSubmit = handleSubmit(async (data) => {
+        try {
+            setLoading(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setLoading(false);
+            setError('An unexpected error occurred!');
+        }
+    });
 
-  return (
-    <div className="max-w-xl space-y-5">
-      {error && (
-        <Callout.Root color="red">
-          <Callout.Icon>
-            <MdErrorOutline />
-          </Callout.Icon>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
+    return (
+        <div className="max-w-xl space-y-5">
+            {error && (
+                <Callout.Root color="red">
+                    <Callout.Icon>
+                        <MdErrorOutline/>
+                    </Callout.Icon>
+                    <Callout.Text>{error}</Callout.Text>
+                </Callout.Root>
+            )}
 
-      <form className="space-y-3" onSubmit={onSubmit}>
-        <TextField.Root>
-          <TextField.Input placeholder="Title" {...register('title')} />
-        </TextField.Root>
-        <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
-          )}
-        />
-        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+            <form className="space-y-3" onSubmit={onSubmit}>
+                <TextField.Root>
+                    <TextField.Input placeholder="Title" {...register('title')} />
+                </TextField.Root>
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({field}) => (
+                        <SimpleMDE placeholder="Description" {...field} />
+                    )}
+                />
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button disabled={isLoading}>
-          Submit new issue
-          {isLoading && <Spinner />}
-        </Button>
-      </form>
-    </div>
-  );
+                <Button disabled={isLoading}>
+                    Submit new issue
+                    {isLoading && <Spinner/>}
+                </Button>
+            </form>
+        </div>
+    );
 };
 
 export default NewIssuePage;
