4edb99d354f909b3e6894e487c01a9fe4187b8f5
Keyvan <skhprogramming@gmail.com>
2024-01-31 17:03:18 +0330

    Added delete button layout

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,13 @@
+import { TrashIcon } from '@radix-ui/react-icons';
+import { Button } from '@radix-ui/themes';
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button color="red">
+      <TrashIcon />
+      Delete Issue
+    </Button>
+  );
+};
+
+export default DeleteIssueButton;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+import { NavBar } from '@/app/NavBar';
+import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import './theme-config.css';
-import './globals.css';
 import type { Metadata } from 'next';
 import { Raleway } from 'next/font/google';
-import { Theme } from '@radix-ui/themes';
-import { NavBar } from '@/app/NavBar';
+import './globals.css';
+import './theme-config.css';
 
 const raleway = Raleway({
   subsets: ['latin'],
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body className={raleway.variable}>
         <Theme accentColor="violet" radius="large">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
