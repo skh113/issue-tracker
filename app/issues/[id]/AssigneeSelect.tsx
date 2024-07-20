989d1b311c884e71ce17c098938abafcb252d8e8
Keyvan <skhprogramming@gmail.com>
2024-07-20 16:54:12 +0330

    add: api call to assignee-select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import { Select } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
+import { User } from "@prisma/client";
+import axios from "axios";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Asign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Keyvan</Select.Item>
+          {users.map(({ id, name }) => (
+            <Select.Item key={id} value={id}>
+              {name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
