00d470e4ba568d562dad41a1f280e97adc6f56a8
Keyvan <skhprogramming@gmail.com>
2024-07-20 15:45:24 +0330

    add: drop-down menu in navbar for better experience

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiOutlineBug } from "react-icons/ai";
 import { useSession } from "next-auth/react";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 export function NavBar() {
   const currentPath = usePathname();
@@ -43,7 +50,25 @@ export function NavBar() {
           </Flex>
           <Box>
             {status === "authenticated" ? (
-              <Link href="/api/auth/signout">Log out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    className="cursor-pointer"
+                    size="2"
+                    radius="full"
+                    src={session.user!.image!}
+                    fallback="?"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email!}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             ) : status === "unauthenticated" ? (
               <Link href="/api/auth/signin">Login</Link>
             ) : null}
