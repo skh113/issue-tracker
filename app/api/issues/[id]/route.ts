b7c55a3413536679ba48610f28d9228f1ebffdd9
Keyvan <skhprogramming@gmail.com>
2024-07-21 11:42:54 +0330

    add: new validation for pathing issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
@@ -12,7 +12,7 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
@@ -25,7 +25,7 @@ export async function PATCH(
       return NextResponse.json({ error: "Invalid user" }, { status: 400 });
   }
 
-  const issue = await prisma?.issue.findUnique({
+  const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
   if (!issue)
