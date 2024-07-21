243e20646b9e0cccfe12738d076f4aa0f4aa7776
Keyvan <skhprogramming@gmail.com>
2024-07-21 12:15:35 +0330

    fix: increased use-users stale time

--- app/hooks/useUsers.ts
+++ app/hooks/useUsers.ts
@@ -6,7 +6,7 @@ const useUsers = () =>
   useQuery({
     queryKey: ["users"],
     queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, //60s
+    staleTime: 60 * 60 * 1000, // 1h
     retry: 3,
   });
 
