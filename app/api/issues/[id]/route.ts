9e590d76b62c76c50139c690917e675b81763419
Keyvan <skhprogramming@gmail.com>
2024-07-21 11:26:33 +0330

    refactor: validation for updating issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -16,6 +16,15 @@ export async function PATCH(
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
+  const { assignedToUserId, title, description, status } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
+  }
+
   const issue = await prisma?.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -25,9 +34,10 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
-      status: body.status,
+      title,
+      description,
+      status,
+      assignedToUserId,
       updatedAt: new Date(),
     },
   });
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,8 +1,24 @@
-import { Status } from '@prisma/client';
-import { z } from 'zod';
+import { Status } from "@prisma/client";
+import { z } from "zod";
 
 export const issueSchema = z.object({
-  title: z.string().min(1, 'Title is required!').max(255),
-  description: z.string().min(1, 'Description is required!'),
-  status: z.enum([Status.CLOSED, Status.IN_PROGRESS, Status.OPEN])
+  title: z.string().min(1, "Title is required!").max(255),
+  description: z.string().min(1, "Description is required!").max(65535),
+  status: z.enum([Status.CLOSED, Status.IN_PROGRESS, Status.OPEN]),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required!").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required!")
+    .max(65535)
+    .optional(),
+  status: z.enum([Status.CLOSED, Status.IN_PROGRESS, Status.OPEN]).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required!")
+    .max(255)
+    .optional()
+    .nullable(),
 });
